95286ddb8ad5b87ae36d28c51f388ebf0f87532b
Mindaugas Jasiulis <jasius@outlook.com>
2021-06-07 11:16:13 +0300

    moved toxicity evaluation into a separate function

--- src/api/perspective.js
+++ src/api/perspective.js
@@ -1,5 +1,42 @@
 const fetch = require('request-promise-native');
 
+async function getToxicity (rawMessage, message, dataCollection) {
+    const getSanitizedEmojis = new RegExp(/<((!?\d+)|(:.+?:\d+))>/g),
+        getSanitizedMentions = new RegExp(/<!?(@|@!|@&)(\d+)>/g),
+        getSanitizedChannels = new RegExp(/<!?#(\d+)>/g),
+        names = ['Jack', 'Jennifer'];
+    let unsanitizedMessage = rawMessage.replace(/([\u2011-\u27BF]|[\uE000-\uF8FF]|[\uD800-\uDBFF][\uDC00-\uDFFF])/g, '')
+    unsanitizedMessage = unsanitizedMessage.replace(getSanitizedEmojis, '');
+    unsanitizedMessage = unsanitizedMessage.replace(getSanitizedChannels, '#' + message.channel.name);
+    const sanitizedMessage = unsanitizedMessage.replace(getSanitizedMentions, names[Math.floor(Math.random() * 2)]);
+
+    if (!sanitizedMessage.startsWith(message.server.prefix) && !sanitizedMessage.startsWith('>') && sanitizedMessage.length !== 0) {
+        try {
+            const result = await fetch({
+                method: 'POST',
+                uri: `https://commentanalyzer.googleapis.com/v1alpha1/comments:analyze?key=${process.env.PERSPECTIVE_KEY}`,
+                body: {
+                    comment: {
+                        text: sanitizedMessage,
+                        type: 'PLAIN_TEXT'
+                    },
+                    languages: ['en'],
+                    requestedAttributes: { SEVERE_TOXICITY: {}, INSULT: {} },
+                    doNotStore: dataCollection,
+                    communityId: `${message.server.name}/${message.channel.name}`
+                },
+                json: true
+            })
+            return { toxicity: result.attributeScores.SEVERE_TOXICITY.summaryScore.value, insult: result.attributeScores.INSULT.summaryScore.value, combined: (result.attributeScores.SEVERE_TOXICITY.summaryScore.value + result.attributeScores.INSULT.summaryScore.value) / 2 }
+        } catch (e) {
+            console.error(e)
+            return { toxicity: NaN, insult: NaN, combined: NaN }
+        }
+    } else {
+        return { toxicity: NaN, insult: NaN, combined: NaN }
+    }
+}
+
 async function sendFeedback (attribute, comment, suggestedScore, messageContext) {
     const requestBody = {
         method: 'POST',
@@ -47,4 +84,4 @@ async function sendFeedback (attribute, comment, suggestedScore, messageContext)
     }
 }
 
-module.exports = { sendFeedback }
+module.exports = { sendFeedback, getToxicity }
