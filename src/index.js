2da0ec7f1593bee9d4ca8bc24d578fc9e81ef611
Mindaugas Jasiulis <jasius@outlook.com>
2021-08-08 23:46:46 +0300

    removed uptime from status

--- src/index.js
+++ src/index.js
@@ -50,17 +50,11 @@ let watchedKeywordsCollection = db.getWatchedKeywords(),
 
 client.on('ready', () => {
   console.info(chalk.green(`Logged in as ${client.user.tag}!`));
-  client.user.setActivity(`Dotsimus.com traffic`, { type: 'WATCHING' });
-  let hours = 0;
-  setInterval(async () => {
-    hours += 1;
-    await client.user.setActivity(`Dotsimus.com for ${hours} hour(s)`, { type: 'WATCHING' });
-  }, 3600000);
+  client.user.setActivity(`Dotsimus.com`, { type: 'WATCHING' });
   refreshServersConfigListing()
   // client.api.applications('731190736996794420').guilds('553939036490956801').commands('792118637808058408').delete()
   // client.api.applications('731190736996794420').guilds('553939036490956801').commands.get().then(data => console.log(data))
-  client.ws.on("INTERACTION_CREATE", async (interaction) => {
-    try {
+});
       client.commands.get(interaction.data.name)?.execute(client, interaction, activeUsersCollection);
     } catch (error) {
       console.error(error);
