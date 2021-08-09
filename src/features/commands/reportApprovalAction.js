b68e2e67e3fcbd6456c553da09ce1d064af528f7
Mindaugas Jasiulis <jasius@outlook.com>
2021-08-09 22:57:09 +0300

    updated mute role copy writing

--- src/features/commands/reportApprovalAction.js
+++ src/features/commands/reportApprovalAction.js
@@ -8,10 +8,13 @@ module.exports = {
     async execute (client, interaction) {
         if (interaction.member.permissions.serialize().KICK_MEMBERS || interaction.member.permissions.serialize().BAN_MEMBERS) {
             const removedMessageInfo = interaction.message.embeds[0].footer.text.split(/ +/g)
+            let reportApprovalEphemeralMsg = 'Report approved, user is notified & muted.'
             client.guilds.cache.get(interaction.guildId).members.fetch(interaction.message.embeds[0].fields[4].value).then(async member => {
-                 member.roles.add(await db.getAlerts(interaction.guildId).then(alerts => {
+                member.roles.add(await db.getAlerts(interaction.guildId).then(alerts => {
                     return alerts[0].mutedRoleId
                 }));
+            }).catch(err => {
+                reportApprovalEphemeralMsg = 'Report approved, user is notified, but not muted. Please set up muted role.'
             })
             db.saveMessage(
                 +new Date,
@@ -39,7 +42,7 @@ module.exports = {
                                 message.edit('Message removed, report verified by the moderation team.')
                                 interaction.reply({
                                     ephemeral: true,
-                                    content: 'Report approved, user is muted.'
+                                    content: reportApprovalEphemeralMsg
                                 });
                             })
                     })
@@ -54,7 +57,7 @@ module.exports = {
                     components: []
                 })
         } else {
-            interaction.reply({ ephemeral: true, content: Math.random() < 0.9 ? 'You do not have permission to use this command.' : 'You nasty devil, you don\'t take no for an answer?'})
+            interaction.reply({ ephemeral: true, content: Math.random() < 0.9 ? 'You do not have permission to use this command.' : 'You nasty devil, you don\'t take no for an answer?' })
         }
     },
 };
