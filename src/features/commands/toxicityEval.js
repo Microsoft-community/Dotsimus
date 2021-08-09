2b97770a58e6321751b5fd81274bf5420dfc3c2f
Mindaugas Jasiulis <jasius@outlook.com>
2021-08-09 08:27:06 +0300

    v13 support for toxicity command

--- src/features/commands/toxicityEval.js
+++ src/features/commands/toxicityEval.js
@@ -1,47 +1,37 @@
-const Discord = require('discord.js'),
+const { MessageEmbed } = require('discord.js'),
     perspective = require('../../api/perspective');
 
 module.exports = {
     name: 'toxic',
     description: 'Evaluates message\'s toxicity with the power of machine learning.',
     execute (client, interaction) {
-        console.log(interaction);
-        perspective.getToxicity(interaction.data.options[0].value, {
+        perspective.getToxicity(interaction.options._hoistedOptions[0].value, {
             channel: {
                 name: interaction.channel_id
             },
             server: {
-                prefix: interaction.guild_id, name: interaction.guild_id
+                prefix: interaction.guildId, name: interaction.guildId
             }
         }, true).then(toxicity => {
             const messageToxicity = toxicity.toxicity
             if (!isNaN(messageToxicity)) {
-                const embedResponse = new Discord.MessageEmbed()
+                const embedResponse = new MessageEmbed()
                     .setColor('#ffbd2e')
                     .addFields(
-                        { name: 'Message', value: `||${interaction.data.options[0].value.slice(0, 1020)}||` },
+                        { name: 'Message', value: `||${interaction.options._hoistedOptions[0].value.slice(0, 1020)}||` },
                         { name: 'Probability', value: `**Toxicity:** ${toxicity.toxicity} \n **Insult:** ${toxicity.insult}` },
-                        { name: 'Dotsimus combined probability', value: toxicity.combined }
+                        { name: 'Dotsimus combined probability', value: `${toxicity.combined}` }
                     );
-                client.api.interactions(interaction.id, interaction.token).callback.post({
-                    data: {
-                        type: 4,
-                        data: {
-                            embeds: [embedResponse]
-
-                        },
-                    },
-                });
+                interaction.reply({
+                    type: 4,
+                    embeds: [embedResponse]
+                })
             } else {
-                client.api.interactions(interaction.id, interaction.token).callback.post({
-                    data: {
-                        type: 4,
-                        data: {
-                            flags: 64,
-                            content: 'Message cannot be analyzed.'
-                        },
-                    },
-                });
+                interaction.reply({
+                    type: 4,
+                    ephemeral: true,
+                    content: 'Provided message cannot be analyzed.'
+                })
             }
         })
     },
