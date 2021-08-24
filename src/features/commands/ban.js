d2dc5c21e744bf19f31e5a250e37a2e1ff0370e9
itsWindows11 <81253203+itsWindows11@users.noreply.github.com>
2021-08-24 11:13:35 -0700

    Add ban and unban cmd files

---
+++ src/features/commands/ban.js
@@ -0,0 +1,47 @@
+const { SlashCommandBuilder } = require('@discordjs/builders'),
+    { MessageEmbed } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('ban')
+        .setDescription('Bans a user from the server.')
+        .addUserOption(option =>
+            option.setName('user')
+                .setDescription('User to ban.')
+                .setRequired(true)
+        )
+        .addStringOption(option =>
+            option.setName('reason')
+                .setDescription('Reason to include in ban message')
+                .setRequired(false)
+        ),
+    async execute(client, interaction) {
+        await interaction.deferReply();
+        var userSnowflake = interaction.options.getUser('user');
+
+        if (!isNaN(userSnowflake)) {
+            var reason = interaction.options.get('reason')?.value;
+            // Ban user from guild
+            interaction.guild.members.ban(user);
+            if (isNaN(reason)) {
+                reason = "No reason specified";
+                interaction.editReply({
+                    type: 4,
+                    content: `<@${userSnowflake}> has been banned`
+                });
+            } else {
+                interaction.editReply({
+                    type: 4,
+                    content: `<@${userSnowflake}> has been banned for **${reason}**`
+                });
+            }
+
+        } else {
+            interaction.editReply({
+                type: 4,
+                ephemeral: true,
+                content: `There's no user mentioned, try mentioning a user in the option \`user\` then try again`
+            })
+        }
+    },
+}
---
+++ src/features/commands/unban.js
@@ -0,0 +1,32 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('unban')
+        .setDescription('Unbans a user from the server.')
+        .addStringOption(option =>
+            option.setName('snowflake')
+                .setDescription('User ID (snowflake) to unban.')
+                .setRequired(true)
+        ),
+    async execute(client, interaction) {
+        await interaction.deferReply();
+        var userSnowflake = interaction.options.get('snowflake')?.value;
+
+        if (!isNaN(userSnowflake)) {
+            // Unban user from guild
+            interaction.guild.members.unban(user);
+            interaction.editReply({
+                type: 4,
+                content: `<@${userSnowflake}> has been unbanned successfully`
+            });
+            
+        } else {
+            interaction.editReply({
+                type: 4,
+                ephemeral: true,
+                content: `There's no user mentioned, try mentioning a user ID/snowflake in the option \`snowflake\` then try again`
+            })
+        }
+    },
+}
