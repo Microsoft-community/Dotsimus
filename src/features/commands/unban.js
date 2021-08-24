a9dec66b5e93ed559ac4dc42ae97a6485e185994
itsWindows11 <81253203+itsWindows11@users.noreply.github.com>
2021-08-24 22:24:18 +0200

    Add permission checks

--- src/features/commands/unban.js
+++ src/features/commands/unban.js
@@ -12,7 +12,9 @@ module.exports = {
     async execute(client, interaction) {
         await interaction.deferReply();
         var userSnowflake = interaction.options.get('snowflake')?.value;
+        var commandUser = client.users.cache.get(interaction.member.user.id);
 
+        if (commandUser.permissions.has("BAN_MEMBERS", true)) {
         if (!isNaN(userSnowflake)) {
             // Unban user from guild
             interaction.guild.members.unban(user);
@@ -28,5 +30,16 @@ module.exports = {
                 content: `There's no user mentioned, try mentioning a user ID/snowflake in the option \`snowflake\` then try again`
             })
         }
+       } else {
+       interaction.editReply({
+
+                type: 4,
+
+                ephemeral: true,
+
+                content: `You don't have enough permissions to run this command`
+
+            })
+       }
     },
 }
