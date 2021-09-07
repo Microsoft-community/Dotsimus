e94b1d38e6ed2ea96acab9db5253a2b70622c3fa
itsWindows11 <81253203+itsWindows11@users.noreply.github.com>
2021-09-07 13:41:53 +0200

    Make it so that constant "keyword" throws an error when it's null

--- src/features/commands/unwatch.js
+++ src/features/commands/unwatch.js
@@ -11,7 +11,7 @@ module.exports = {
                 .setDescription('The keyword you will untrack.')
         ),
     async execute (client, interaction) {
-        const keyword = interaction.options.getString("keyword");
+        const keyword = interaction.options.getString("keyword", true);
         const { guild: server } = interaction;
         let watchedKeywordsCollection = db.getWatchedKeywords();
         const refreshWatchedCollection = () => (
