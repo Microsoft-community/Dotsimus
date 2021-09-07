a33d871b04b6e9558211ada7c4d4b52ac7e86baa
itsWindows11 <81253203+itsWindows11@users.noreply.github.com>
2021-09-07 13:32:30 +0200

    Add keyword that the user typed into message

--- src/features/commands/unwatch.js
+++ src/features/commands/unwatch.js
@@ -12,7 +12,7 @@ module.exports = {
         ),
     async execute (client, interaction) {
         const keyword = interaction.options.getString("keyword");
-        const server = interaction.guild;
+        const { guild: server } = interaction;
         let watchedKeywordsCollection = db.getWatchedKeywords();
         const refreshWatchedCollection = () => (
              watchedKeywordsCollection = db.getWatchedKeywords()
@@ -22,13 +22,14 @@ module.exports = {
              // remove one keyword
              db.removeWatchedKeyword1(interaction.member.id, server.id, keyword).then(resp => {
                  refreshWatchedCollection();
-                 interaction.reply({ content: `Keyword removed successfully.`, ephemeral: true });
+                 interaction.reply({ content: `Keyword "${keyword}" removed successfully.`, ephemeral: true });
              });
            } catch (error) {
               interaction.reply({ content: `Something went horribly wrong while removing keyword.`, ephemeral: true });
            }
          } else {
             try {
+              // remove all keywords
               db.removeWatchedKeyword(interaction.member.id, server.id).then(resp => {
                  refreshWatchedCollection();
                  interaction.reply({ content: `All keywords you have watched before has been removed successfully.`, ephemeral: true });
