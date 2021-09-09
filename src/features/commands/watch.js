0b88f28d4cfef23800f9c17eded027bf41cbc7e9
itsWindows11 <81253203+itsWindows11@users.noreply.github.com>
2021-09-09 10:04:01 +0200

    Update watch.js

--- src/features/commands/watch.js
+++ src/features/commands/watch.js
@@ -14,6 +14,12 @@ module.exports = {
     async execute (client, interaction) {
         const keyword = interaction.options.getString("keyword", true);
         const {guild: server} = interaction;
+          
+        if (!server) {
+             interaction.reply({ content: "You can only watch keywords in guilds." });
+             return;
+        }
+          
         let watchedKeywordsCollection = db.getWatchedKeywords();
         const refreshWatchedCollection = () => (
              watchedKeywordsCollection = db.getWatchedKeywords()
