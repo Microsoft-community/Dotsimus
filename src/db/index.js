96cbd9f0e7119a71599ac3b769f5638e55c190a3
itsWindows11 <81253203+itsWindows11@users.noreply.github.com>
2021-09-10 12:26:35 +0200

    Add "unwatch single keyword" method

--- src/db/index.js
+++ src/db/index.js
@@ -116,6 +116,23 @@ module.exports = {
       throw 'Failed to add a tracked word.'
     })
   },
+  removeWatchedKeyword1: function (userId, serverId, watchedWords) {
+    const query = { userId, serverId }
+    return new Promise((resolve, reject) => {
+      WatchKeyword.findOneAndUpdate(query, {
+        $pull: { 'watchedWords': { $in: [watchedWords] } }
+      }, (error, data) => {
+        if (error) {
+          console.error(error)
+          return reject(error)
+        }
+        resolve(data)
+      })
+    }).catch(e => {
+      console.error(e)
+      throw 'Failed to remove a tracked word.'
+    })
+  },
   removeWatchedKeyword: function (userId, serverId, watchedWord) {
     const query = { userId, serverId }
     return new Promise((resolve, reject) => {
