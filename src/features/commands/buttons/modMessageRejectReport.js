166f43eb407228f584464f9cef9b217870fc964d
li <li>
2021-09-12 23:41:58 +0200

    Fixed exception on report rejection

--- src/features/commands/buttons/modMessageRejectReport.js
+++ src/features/commands/buttons/modMessageRejectReport.js
@@ -21,7 +21,6 @@ module.exports = {
             return;
         }
 
-        reportedContent = MessageReports.ReportedMessage.fromJSON(reportData.reportedContent);
         reportObject = MessageReports.parseReport(client, reportData);
 
         for (let i = 0; i < reportObject.signalers.length; i++) {
