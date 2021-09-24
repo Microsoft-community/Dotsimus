fd24110469a5f2644268d8d04e47d784d4eb324f
Koji <cldoc507@outlook.com>
2021-09-24 12:39:03 -0400

    Update report.js

--- src/features/commands/report.js
+++ src/features/commands/report.js
@@ -69,7 +69,7 @@ module.exports = {
         }
         if (reportedMessage.author.bot) {
             return await interaction.reply({
-                content: 'Message reports can only be used on members.',
+                content: 'Message reports can only be used on bots.',
                 ephemeral: true
             });
         }
