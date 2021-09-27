42f34bbfd09942a4a9c834d8e6a4b05596f9d062
Mindaugas Jasiulis <jasius@outlook.com>
2021-09-27 23:55:24 +0300

    disabled flip-a-coin feature for msft

--- src/features/commands/flip.js
+++ src/features/commands/flip.js
@@ -13,6 +13,12 @@ module.exports = {
                 .addChoice('Head', 'head')
                 .addChoice('Tails', 'tails')),
     async execute (client, interaction) {
+        if (interaction.guild.id === '150662382874525696') {
+            return interaction.reply({
+                content: 'This functionality is currently disabled on this server, learn more over at [Dotsimus.com](https://dotsimus.com/).',
+                ephemeral: true
+            });
+        }
         const result = Math.random() >= 0.5 ? 'Sorry buddy, you got this wrong this time around! Better luck next time.' : 'Winner winner, artificial dinner! You got it right, keep it up champ!';
         await interaction.deferReply();
         await wait(2000);
