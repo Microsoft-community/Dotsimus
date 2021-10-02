cb6dd226be3ef19eb5d19a2bf96a7e99a2b29b3a
li <li>
2021-10-02 17:56:36 +0200

    Made both font and font size reduction test configurable

--- src/utils/dotdraw.js
+++ src/utils/dotdraw.js
@@ -15,7 +15,7 @@ async function drawDot(text, color = [0, 0, 0]) {
     const context = canvas.getContext('2d');
     context.drawImage(template, 0, 0, template.width, template.height);
 
-    // return the dimension for writing the text
+    // calculate the dimension for writing the text
     const boxArea = getBoxArea(canvas, template);
     const boxDelta = getBoxDelta(boxArea);
 
@@ -23,7 +23,7 @@ async function drawDot(text, color = [0, 0, 0]) {
     const htmlColor = rgbToHex(color);
 
     dimensions = [];
-    context.font = getFontSize(context, boxDelta, 'sans-serif', text, dimensions);
+    context.font = getFontSize(context, boxDelta, config.font, text, dimensions);
     context.fillStyle = htmlColor;
 
     // calculate the position to make the text centered to the box area
@@ -37,18 +37,18 @@ async function drawDot(text, color = [0, 0, 0]) {
 function calculateCenter(boxDelta, dimensions) {
     const widthRemaining = boxDelta[0] - dimensions[0];
     const heightRemaining = boxDelta[1] - dimensions[1];
+    const heightOffset = dimensions[1] - dimensions[2];
 
-    let widthDeltaCenter = widthRemaining / 2.0;
-    let heightDeltaCenter = heightRemaining / 2.0 + dimensions[1] - dimensions[2];
-
-    if (widthDeltaCenter > boxDelta[0]) widthDeltaCenter = boxDelta[0];
-    if (heightDeltaCenter > boxDelta[1]) heightDeltaCenter = boxDelta[1];
+    // position of characters starts from the left bottom
+    const widthDeltaCenter = widthRemaining / 2.0;
+    const heightDeltaCenter = heightRemaining / 2.0 + heightOffset;
 
     return [widthDeltaCenter, heightDeltaCenter];
 }
 
 function getFontSize(context, boxDelta, font, text, dimensions) {
     // get the starting font size
+    // the text must fit both horizontally and vertically
     const maxFontSize = boxDelta[0] < boxDelta[1] ? boxDelta[0] : boxDelta[1];
     let fontSize = maxFontSize;
     let nextSize = fontSize;
@@ -57,14 +57,16 @@ function getFontSize(context, boxDelta, font, text, dimensions) {
     do {
         fontSize = nextSize;
         context.font = `${fontSize}px ${font}`;
-        nextSize -= 10;
+        nextSize -= config.reduceSize;
 
         measured = context.measureText(text);
+        // calculate the height bound
         measuredHeight = measured.actualBoundingBoxAscent + measured.actualBoundingBoxDescent
     } while (measured.width > boxDelta[0] || measuredHeight > boxDelta[1]);
 
     dimensions[0] = measured.width;
     dimensions[1] = measuredHeight;
+    // sometimes letters like 'p' are a bit off center, so keep the 'descent' information
     dimensions[2] = measured.actualBoundingBoxDescent;
 
     return context.font;
@@ -87,6 +89,7 @@ function getBoxArea(canvas, template) {
         const heightRatio = canvas.height / template.height;
 
         let boxArea = [[config.boxArea[0][0], config.boxArea[0][1]], [config.boxArea[1][0], config.boxArea[1][1]]];
+        // resize the area to the canvas
         boxArea[0][0] *= widthRatio;
         boxArea[1][0] *= widthRatio;
         boxArea[0][1] *= heightRatio;
@@ -106,7 +109,9 @@ function getBoxDelta(boxArea) {
 const config = {
     template: '../../assets/img/template.png',
     // [minArea] [maxArea]
-    boxArea: [[775, 75], [1455, 530]]
+    boxArea: [[775, 75], [1455, 530]],
+    reduceSize: 10,
+    font: 'sans-serif'
 }
 
 module.exports = {
