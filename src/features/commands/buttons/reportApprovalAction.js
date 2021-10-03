718fd95da86234030e100e6d4454e6555b53eca7
Mindaugas Jasiulis <jasius@outlook.com>
2021-10-03 23:23:47 +0300

    added notifications for investigation CTA actions, changed embed color for 2nd approval action

--- src/features/commands/buttons/reportApprovalAction.js
+++ src/features/commands/buttons/reportApprovalAction.js
@@ -1,4 +1,4 @@
-const { Client } = require('discord.js'),
+const { Client, MessageEmbed, MessageActionRow, MessageButton } = require('discord.js'),
     db = require('../../../db'),
     perspective = require('../../../api/perspective');
 
@@ -48,10 +48,31 @@ module.exports = {
                 .catch(console.error);
             const updatedEmbed = interaction.message.embeds[0]
                 .setColor('#32CD32')
-                .setFooter('');
+                .setFooter(''),
+                actionMessage = `Report approved by <@${interaction.member.id}>`,
+                investigationNoticeButtons = new MessageActionRow()
+                    .addComponents(
+                        new MessageButton()
+                            .setLabel('Open thread')
+                            .setURL(interaction.message.url)
+                            .setStyle('LINK'));
+
+            db.getAlerts(interaction.guild.id).then(alerts => alerts.forEach(alert => {
+                const sendMessageToChannel = async (channelId) => {
+                    const channel = await client.guilds.cache.get(interaction.guildId).channels.fetch(channelId),
+                        investigationNotice = new MessageEmbed()
+                            .setColor('#32CD32')
+                            .setTitle('Report approved, user is muted indefinitely')
+                            .addField('Message', interaction.message.embeds[0].fields[0].value)
+                            .addField('User', interaction.message.embeds[0].fields.filter(field => field.name === 'User').map(field => field.value)[0])
+                            .setDescription(`Approved by <@${interaction.member.id}>`);
+                    channel.send({ embeds: [investigationNotice], components: [investigationNoticeButtons] }).catch(console.error);
+                }
+                sendMessageToChannel(alert.channelId);
+            }));
             interaction.message.edit(
                 {
-                    content: `Report approved by <@${interaction.member.id}>`,
+                    content: actionMessage,
                     embeds: [updatedEmbed],
                     components: []
                 })
