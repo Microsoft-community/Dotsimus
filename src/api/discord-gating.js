b4615a07e2ecc2687df01ea5af270f12e1aa9fd7
Nikhil B <heftymausnik@gmail.com>
2021-10-17 14:09:42 +0530

    changed export

--- src/api/discord-gating.js
+++ src/api/discord-gating.js
@@ -6,7 +6,7 @@ const { REST } = require('@discordjs/rest');
 // ~15mins caching would be great, wouldn't it?
 // it'd also be kinda neat if this could be done with discord.js rest and routes
 // update: nvm
-module.exports.fetchRules = async (guildID) => {
+async function fetchRules(guildID) {
     const rest = new REST({ version: '9' }).setToken(process.env.DEVELOPMENT !== 'true' ? process.env.BOT_TOKEN : process.env.BOT_TOKEN_DEV);
 
     try {
@@ -17,3 +17,5 @@ module.exports.fetchRules = async (guildID) => {
         return 0;
     }
 }
+
+module.exports = { fetchRules }
