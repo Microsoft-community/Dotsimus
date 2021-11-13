bfcc0a4d8295ced1a054a5f97253c11e339b81c3
Mindaugas Jasiulis <jasius@outlook.com>
2021-11-13 07:09:56 +0200

    stop message evaluation if language isn't English

--- src/index.js
+++ src/index.js
@@ -260,6 +260,7 @@ client.on('messageCreate', message => {
   })
   if (server.isPremium && !(message.member.permissions.has(Permissions.FLAGS.KICK_MEMBERS) || message.member.permissions.has(Permissions.FLAGS.BAN_MEMBERS) || message.member.roles.cache.has('332343869163438080')) || process.env.DEVELOPMENT === 'true') {
     getToxicity(message.content, message, false).then(toxicity => {
+      if (!toxicity.isSupportedLanguage) return;
       // console.info(`#${message.channel.name} ${message.author.username}: ${message.content} | ${chalk.red((Number(toxicity.toxicity) * 100).toFixed(2))} ${chalk.red((Number(toxicity.insult) * 100).toFixed(2))}`)
       const messageToxicity = toxicity.toxicity;
       const alerts = async (messages) => {
