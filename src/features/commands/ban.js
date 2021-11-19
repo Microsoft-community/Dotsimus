fb8eb952c851d1d5e871c9fcdbaa2ee074921f91
SimpleBear in developer mode <81253203+itsWindows11@users.noreply.github.com>
2021-11-19 11:28:53 +0200

    Ban command improvements

--- src/features/commands/ban.js
+++ src/features/commands/ban.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { Permissions, MessageAttachment } = require("discord.js");
 
 module.exports = {
     type: 'slash',
@@ -16,44 +17,57 @@ module.exports = {
                 .setRequired(false)
         ),
     async execute (client, interaction) {
-        const userSnowflake = interaction.options.getUser('user');
-        if (interaction.member.permissions.serialize().BAN_MEMBERS) {
-            if (!isNaN(userSnowflake)) {
-                const reason = interaction.options.get('reason')?.value ?? "No reason specified.";
-                interaction.guild.members.ban(userSnowflake, { reason: interaction.options.get('reason')?.value }).then(() => {
-                    if (!interaction.options.get('reason')?.value) {
-                        interaction.reply({
-                            type: 4,
-                            content: `${userSnowflake} has been banned.`,
-                            ephemeral: false
-                        });
-                    } else {
-                        interaction.reply({
-                            type: 4,
-                            content: `${userSnowflake} has been banned for **${reason}.**`,
-                            ephemeral: false
-                        });
-                    }
-                }).catch(() => {
-                    interaction.reply({
-                        content: 'Something went horribly wrong, check whether bot has required permissions enabled.',
-                        ephemeral: true
-                    })
-                })
-            } else {
-                interaction.reply({
-                    type: 4,
-                    ephemeral: true,
-                    content: `⚠️ Invalid user specified, double check whether user ID is correct.`
-                })
+        const user = interaction.options.getUser('user');
+
+        if (!interaction.member.permissions.has(Permissions.FLAGS.BAN_MEMBERS)) {
+            const ohSimusAsset = new MessageAttachment('./src/assets/images/ohsimus.png');
+            interaction.reply({
+                content: 'Oh snap! You don\'t have sufficient permissions to execute this command.',
+                files: [ohSimusAsset],
+                ephemeral: true
+            });
+            return;
+        }
+
+        if (interaction.options.getMember('user')) {
+            if (interaction.options.getMember('user').manageable) {
+                banUser(user, interaction);
+                return;
             }
+            const ohSimusAsset = new MessageAttachment('./src/assets/images/ohsimus.png');
+            interaction.reply({
+                content: 'Oh snap! You don\'t have sufficient permissions to ban this user.',
+                files: [ohSimusAsset],
+                ephemeral: true
+            });
+            return;
+        }
+        
+        banUser(user, interaction);
+    },
+}
+
+var banUser = (user, interaction) => {
+    const reason = interaction.options.get('reason')?.value ?? "No reason specified";
+    user.send({ content: `You have been banned from ${interaction.guild.name} for **${reason}**.` });
+    interaction.guild.members.ban(user, { reason: interaction.options.getString('reason')?.value }).then(() => {
+        if (!interaction.options.get('reason')?.value) {
+            interaction.reply({
+                type: 4,
+                content: `${user} has been banned.`,
+                ephemeral: false
+            });
         } else {
             interaction.reply({
                 type: 4,
-                ephemeral: true,
-                content: `You don't have required permissions to run this command.`
-            })
-
+                content: `${user} has been banned for **${reason}**.`,
+                ephemeral: false
+            });
         }
-    },
+    }).catch(() => {
+        interaction.reply({
+            content: 'Something went horribly wrong, check whether bot has required permissions enabled.',
+            ephemeral: true
+        })
+    });
 }
