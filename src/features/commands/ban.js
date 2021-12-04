33982d0c613565f9ee97654913b64db8358f2f9b
SimpleBear in developer mode <81253203+itsWindows11@users.noreply.github.com>
2021-12-04 23:21:37 +0200

    Use one, unified ohsimus constant instead of redeclaring

--- src/features/commands/ban.js
+++ src/features/commands/ban.js
@@ -18,9 +18,9 @@ module.exports = {
         ),
     async execute (client, interaction) {
         const user = interaction.options.getUser('user');
+        const ohSimusAsset = new MessageAttachment('./src/assets/images/ohsimus.png');
 
         if (interaction.guild.id === '150662382874525696') {
-            const ohSimusAsset = new MessageAttachment('./src/assets/images/ohsimus.png');
             return interaction.reply({
                 content: 'Oh snap! This feature is currently disabled by the moderation team.',
                 files: [ohSimusAsset],
@@ -29,7 +29,6 @@ module.exports = {
         }
         
         if (!interaction.member.permissions.has(Permissions.FLAGS.BAN_MEMBERS)) {
-            const ohSimusAsset = new MessageAttachment('./src/assets/images/ohsimus.png');
             interaction.reply({
                 content: 'Oh snap! You don\'t have sufficient permissions to execute this command.',
                 files: [ohSimusAsset],
@@ -43,7 +42,6 @@ module.exports = {
                 banUser(user, interaction);
                 return;
             }
-            const ohSimusAsset = new MessageAttachment('./src/assets/images/ohsimus.png');
             interaction.reply({
                 content: 'Oh snap! Dotsimus does not have sufficient permissions to ban this user.\nCheck whether the bot has enough permissions and try again.',
                 files: [ohSimusAsset],
@@ -75,7 +73,8 @@ var banUser = (user, interaction) => {
         }
     }).catch(() => {
         interaction.reply({
-            content: 'Something went horribly wrong, check whether bot has required permissions enabled.',
+            content: 'Oh snap! Something went horribly wrong, check whether bot has required permissions enabled.',
+            files: [ohSimusAsset],
             ephemeral: true
         })
     });
