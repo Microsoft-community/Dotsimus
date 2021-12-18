123a3f194160b6e7aa1029fb9635ed1c07fcc92a
Mindaugas Jasiulis <jasius@outlook.com>
2021-12-18 23:08:45 +0200

    updated text and added embed

--- src/features/commands/premium.js
+++ src/features/commands/premium.js
@@ -1,31 +1,31 @@
 const { SlashCommandBuilder } = require('@discordjs/builders')
-const { Permissions } = require('discord.js');
+const { Permissions, MessageEmbed } = require('discord.js');
 const db = require('../../db');
 
 module.exports = {
     type: 'slash',
     data: new SlashCommandBuilder()
         .setName("premium")
-        .setDescription('Configure premium servers')
+        .setDescription('Configure premium servers.')
         .addSubcommand(subcommand =>
             subcommand
                 .setName("configure")
                 .setDescription("Configure premium features.")
-                .addRoleOption(option => 
+                .addRoleOption(option =>
                     option
                         .setName("role")
-                        .setDescription("The role that will be assigned to toxic members.")
+                        .setDescription("Muted role that will be assigned to flagged members.")
                         .setRequired(true))
                 .addChannelOption(option =>
                     option
                         .setName("channel")
-                        .setDescription("The investigations channel.")
+                        .setDescription("Channel where flagged messages and users will be shown.")
                         .setRequired(true)))
         .addSubcommand(subcommand =>
             subcommand
                 .setName('disable')
                 .setDescription('Disables all premium features.')),
-    async execute(_, interaction) {
+    async execute (_, interaction) {
         if (!interaction.guild) {
             return interaction.reply({ content: 'You can only use this command in servers!', ephemeral: true });
         }
@@ -53,7 +53,11 @@ module.exports = {
                     const role = interaction.options.getRole("role"), channel = interaction.options.getChannel("channel");
                     await db.saveAlert(interaction.guild.id, role.id, undefined, 0.6, channel.id);
                     await channel.send({
-                        content: `This channel has been setup for Dotsimus reports.\n**Muted role**: <@&${role.id}>.`,
+                        embeds: [new MessageEmbed()
+                            .setColor('#0099ff')
+                            .setTitle('Dotsimus Reports')
+                            .setDescription(`This channel has been setup for Dotsimus reports.`)
+                            .addField('Muted role', `<@&${role.id}>`, false)],
                         allowedMentions: { parse: [] }
                     })
                     return interaction.reply({
@@ -70,7 +74,7 @@ module.exports = {
         } catch (e) {
             console.log(e);
             return interaction.reply({
-                content: `Unfortunately, an error occured."`,
+                content: `Error occured during the setup."`,
                 ephemeral: true
             });
         }
